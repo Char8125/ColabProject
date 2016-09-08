5bcd7dc0e703fe1ec915f41b19be418882333380
Char8125 <chris2pherancheta@yahoo.com>
2016-09-09 10:11:32 +1200

    Add files via upload

--- js/examples.js
+++ js/examples.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 $(document).ready(function(){
 	$("#demosMenu").change(function(){
 	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
 	});
-=======
-$(document).ready(function(){
-	$("#demosMenu").change(function(){
-	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
-	});
->>>>>>> origin/master
 });
