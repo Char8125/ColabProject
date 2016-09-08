b165b30c1e820e209a537b41efe364b076ae8183
Char8125 <chris2pherancheta@yahoo.com>
2016-09-09 09:41:25 +1200

    Update

--- js/examples.js
+++ js/examples.js
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 $(document).ready(function(){
 	$("#demosMenu").change(function(){
 	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
 	});
+=======
+$(document).ready(function(){
+	$("#demosMenu").change(function(){
+	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
+	});
+>>>>>>> origin/master
 });
